93bdfbbdaa7ca488a2cccd15ac654a9915cd864d
Felipe91v <felipe-viana91@hotmail.com>
2020-01-28 23:22:07 -0300

    criando rota de listagem de devs

--- backend/src/routes.js
+++ backend/src/routes.js
@@ -1,35 +1,9 @@
 const { Router } = require('express');
-const axios = require('axios');
-const Dev = require('./models/Dev');
+const DevController = require('./controllers/DevController');
 
 const routes = Router();
 
-routes.post('/devs', async (request, response) => {
-    const { github_username, techs, latitude, longitude } = request.body;
-
-    const apiresponse = await axios.get(`https://api.github.com/users/${github_username}`); 
-
-    const { name = login, avatar_url, bio } = apiresponse.data;
-
-    const techsArray = techs.split(',').map(tech => tech.trim());
-
-    const location = {
-        type: 'Point',
-        coordinates: [longitude, latitude],
-     };
-
-    const dev = await Dev.create({
-        github_username,
-        name,
-        avatar_url,
-        bio,
-        techs: techsArray,
-        location,
-    })
-
-    console.log(name, avatar_url, bio, github_username);
-
-    return response.json(dev);
-});
+routes.get('/devs', DevController.index);
+routes.post('/devs', DevController.store);
 
 module.exports = routes;
