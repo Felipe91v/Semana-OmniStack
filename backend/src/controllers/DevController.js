de0bdf853d30c3017e56b41e13a5c6d381d17a9e
Felipe91v <felipe-viana91@hotmail.com>
2020-02-14 23:55:25 -0300

    cadastrando dev utilizando axios

--- backend/src/controllers/DevController.js
+++ backend/src/controllers/DevController.js
@@ -16,9 +16,9 @@ module.exports ={
 
         if(!dev) {
     
-        const apiresponse = await axios.get(`https://api.github.com/users/${github_username}`); 
+        const apiResponse = await axios.get(`https://api.github.com/users/${github_username}`); 
     
-        const { name = login, avatar_url, bio } = apiresponse.data;
+        const { name = login, avatar_url, bio } = apiResponse.data;
     
         const techsArray = parseStringAsArray(techs);
     
