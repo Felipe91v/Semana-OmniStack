a9acdd11bc48c39b45feed63edcbefc1e7999ad3
Felipe91v <felipe-viana91@hotmail.com>
2020-03-23 12:22:26 -0300

    estilização do search form/input e  load button

--- mobile/src/routes.js
+++ mobile/src/routes.js
@@ -6,9 +6,28 @@ import Profile from './pages/Profile';
 
 const Routes = createAppContainer(
     createStackNavigator({
-        Main,
-        Profile,
-    })
+        Main: {
+            screen: Main,
+        navigationOptions: {
+            title: 'DevRadar'
+        },
+    },
+        Profile: {
+            screen: Profile,
+            navigationOptions: {
+                title: 'Perfil no Github'
+                }
+            },
+       }, { 
+           defaultNavigationOptions: {
+               headerTintColor: '#FFF',
+               headerBacktitleVisible: false,
+               headerStyle: {
+                   backgroundColor: '#7D40E7',
+               },
+           },
+
+       })
 );
 
 export default Routes;
