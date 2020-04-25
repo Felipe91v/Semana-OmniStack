fcd3a575b4f7185d308d7edbefa47d2d9a4fd430
Felipe91v <felipe-viana91@hotmail.com>
2020-04-25 08:37:27 -0300

    finalizando aplicacao e adicionando websocket

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,9 +1,15 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const cors = require ('cors');
+const http = require('http');
+
 const routes = require('./routes');
+const { setupWebsocket } = require('./websocket');
 
 const app = express();
+const server = http.Server(app);
+
+setupWebsocket(server);
 
 //string de conexão mongoDB
 mongoose.connect('mongodb+srv://felipebd:Seep1914@cluster0-lhblu.mongodb.net/felipebd?retryWrites=true&w=majority', {
@@ -15,4 +21,4 @@ app.use(cors());
 app.use(express.json());
 app.use(routes);
 
-app.listen(3333);
+server.listen(3333);
